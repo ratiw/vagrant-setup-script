70ef78f4d7735d6fe8fe1dac3367898c4ae8f8fc
Rati Wannapanop <rati.wannapanop@gmail.com>
2014-03-16 16:20:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,12 +41,6 @@ sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 sudo sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
 
-echo "-- Installing Memcached --"
-sudo apt-get install -y memcached
-
-echo "-- Installing phpMyAdmin --"
-sudo apt-get install -y phpmyadmin
-
 echo "-- Restarting Apache --"
 sudo service apache2 restart
 
@@ -63,6 +57,9 @@ sudo service apache2 restart
 echo "-- Installing Composer --"
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
+
+echo "-- Installing phpMyAdmin --"
+sudo apt-get install -y phpmyadmin
 
 
 echo ""
