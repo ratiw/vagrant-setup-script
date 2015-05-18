a89a98c45d6ba11d385475e995649f8ffa7f8775
Rati Wannapanop <rati.wannapanop@gmail.com>
2015-05-18 16:47:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,21 +54,11 @@ sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xi
 
 # Base box optimizations
 
-# exit script if not run as root
-if [[ $EUID -ne 0 ]]; then
-  cat <<END
-you need to run this script as the root user
-use :privileged => true in Vagrantfile
-END
-
-  exit 0
-fi
-
 # optimize apt sources to select best mirror
-perl -pi -e 's@^\s*(deb(\-src)?)\s+http://us.archive.*?\s+@\1 mirror://mirrors.ubuntu.com/mirrors.txt @g' /etc/apt/sources.list
+sudo perl -pi -e 's@^\s*(deb(\-src)?)\s+http://us.archive.*?\s+@\1 mirror://mirrors.ubuntu.com/mirrors.txt @g' /etc/apt/sources.list
 
 # update repositories
-apt-get update
+sudo apt-get update
 
 # PHP
 
