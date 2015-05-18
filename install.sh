eec4d5c47802472b0c2b0c251b4f01c6657bbafe
Rati Wannapanop <rati.wannapanop@gmail.com>
2015-05-18 13:12:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,99 @@ echo "-- MySQL stuffs --"
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
+echo ">>> Installing Base Packages"
+
+if [[ -z $1 ]]; then
+    github_url="https://raw.githubusercontent.com/fideloper/Vaprobash/master"
+else
+    github_url="$1"
+fi
+
 echo "-- Installing base packages --"
-sudo apt-get install -y git-core vim curl wget build-essential python-software-properties
+sudo apt-get install -qq vim wget curl unzip git-core ack-grep software-properties-common build-essential python-software-properties
 # Git config and set Owner
-curl -L https://gist.githubusercontent.com/fideloper/3751524/raw/.gitconfig > /home/vagrant/.gitconfig
+curl --silent -L $github_url/helpers/gitconfig > /home/vagrant/.gitconfig
 sudo chown vagrant:vagrant /home/vagrant/.gitconfig
 
+# Common fixes for git
+git config --global http.postBuffer 65536000
+
+# Cache http credentials for one day while pull/push
+git config --global credential.helper 'cache --timeout=86400'
+
+echo ">>> Installing *.xip.io self-signed SSL"
+
+SSL_DIR="/etc/ssl/xip.io"
+DOMAIN="*.xip.io"
+PASSPHRASE="vaprobash"
+
+SUBJ="
+C=US
+ST=Connecticut
+O=Vaprobash
+localityName=New Haven
+commonName=$DOMAIN
+organizationalUnitName=
+emailAddress=
+"
+
+sudo mkdir -p "$SSL_DIR"
+
+sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
+sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
+sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
+
+# Setting up Swap
+
+# Disable case sensitivity
+shopt -s nocasematch
+
+if [[ ! -z $2 && ! $2 =~ false && $2 =~ ^[0-9]*$ ]]; then
+
+  echo ">>> Setting up Swap ($2 MB)"
+
+  # Create the Swap file
+  fallocate -l $2M /swapfile
+
+  # Set the correct Swap permissions
+  chmod 600 /swapfile
+
+  # Setup Swap space
+  mkswap /swapfile
+
+  # Enable Swap space
+  swapon /swapfile
+
+  # Make the Swap file permanent
+  echo "/swapfile   none    swap    sw    0   0" | tee -a /etc/fstab
+
+  # Add some swap settings:
+  # vm.swappiness=10: Means that there wont be a Swap file until memory hits 90% useage
+  # vm.vfs_cache_pressure=50: read http://rudd-o.com/linux-and-free-software/tales-from-responsivenessland-why-linux-feels-slow-and-how-to-fix-that
+  printf "vm.swappiness=10\nvm.vfs_cache_pressure=50" | tee -a /etc/sysctl.conf && sysctl -p
+
+fi
+
+# Enable case sensitivity
+shopt -u nocasematch
+
+# Base box optimizations
+
+# exit script if not run as root
+if [[ $EUID -ne 0 ]]; then
+  cat <<END
+you need to run this script as the root user
+use :privileged => true in Vagrantfile
+END
+
+  exit 0
+fi
+
+# optimize apt sources to select best mirror
+perl -pi -e 's@^\s*(deb(\-src)?)\s+http://us.archive.*?\s+@\1 mirror://mirrors.ubuntu.com/mirrors.txt @g' /etc/apt/sources.list
+
+# update repositories
+apt-get update
 
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
